a54c0b6cd5f3985855b68cc21003069a1651b641
prashantsingh3 <codewith1prashant@gmail.com>
2022-05-28 03:28:27 +0530

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [err, setErr] = useState("");
+
+  const sumOfN = useMemo(() => {
+    return sumN(number);
+  }, [number]);
+  const [counter, setCounter] = useState(false);
+
+  const onChangeHandler = (e) => {
+    setNumber(e.target.value);
+    if (isNaN(e.target.value ) || e.target.value<0) {
+      setErr("Please enter a valid number !");
+    } else {
+      setErr("");
+    }
+    setCounter(false);
+  };
+
+  const counterHander = () => {
+    setCounter(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Sum of all numbers between 1 and N(number entered in Text Field)</h2>
+      <input
+        type="text"
+        placeholder="Enter a number"
+        value={number}
+        onChange={onChangeHandler}
+      ></input>
+      <div>
+        <small style={{ color: "red" }}>{err}</small>
+      </div>
+      <br />
+      Sum:
+      {counter ? <span> {sumOfN}</span> : null}
+      <br />
+      <button onClick={counterHander}>SUM</button>
     </div>
   );
+}
+
+function sumN(number) {
+  let total = 0;
+  for (let i = 1; i <= number; i++) {
+    total = total + i;
+  }
+  return total;
 }
 
 export default App;
